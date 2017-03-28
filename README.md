f732b3a6cf0ff0e1a746e0346476b8c7a8523568
Corey Sery <coreysery@gmail.com>
2017-03-27 22:24:01 -0500

    Changed seed lmao

--- README.md
+++ README.md
@@ -1,60 +1,28 @@
-# JavaScript SCRUM Board // SP17 Software Engineering Project
+# sandbox3
 
-This project was built from Heroku's MEANContactList2 tutorial.
-https://devcenter.heroku.com/articles/mean-apps-restful-api#prerequisites
+This project was generated with the [Angular Full-Stack Generator](https://github.com/DaftMonk/generator-angular-fullstack) version 4.1.4.
 
-## Prerequisites
+## Getting Started
 
-Heroku CLI
+### Prerequisites
 
-`brew install heroku`
+- [Git](https://git-scm.com/)
+- [Node.js and npm](nodejs.org) Node >= 4.x.x, npm >= 2.x.x
+- [Gulp](http://gulpjs.com/) (`npm install --global gulp`)
+- [MongoDB](https://www.mongodb.org/) - Keep a running daemon with `mongod`
 
-Node.js
+### Developing
 
-https://nodejs.org/en/download/
+1. Run `npm install` to install server dependencies.
 
-Angular CLI
+2. Run `mongod` in a separate shell to keep an instance of the MongoDB Daemon running
 
-`npm install -g angular-cli`
+3. Run `gulp serve` to start the development server. It should automatically open the client in your browser when ready.
 
-MongoDB
+## Build & development
 
-`brew install mongodb`
+Run `gulp build` for building and `gulp serve` for preview.
 
-## Installation
+## Testing
 
-Clone the repo into a directory of your choice.
-
-`git clone https://github.com/benminer/JS-Scrum-Board.git`
-
-Navigate to the directory that it was cloned into, then install dependencies.
-
-`npm install`
-
-Log into your Heroku account through terminal to allow commits and pushes.
-
-`heroku login`
-
-## Initialize LocalHost for testing
-
-Make a MongoDB folder in your root directory.
-
-`mkdir -p /data/db`
-
-Navigate to these folders in Finder and change the privileges from "Read Only" to "Read & Write" for Everyone. This allows MongoDB to write to these folders.
-
-Start the local data base while in the project directory.
-
-`mongod`
-
-Now, switch to the develop branch. It contains the proper code for testing locally. Do this by running
-
-`git checkout develop` and then `git pull` while cd'd into the project.
-
-Now, to get the local app running, run
-
-`npm start`
-
-In your internet browser, navigate to http://localhost:8080/.
-
-You should see your changes reflected on refresh.
+Running `npm test` will run the unit tests with karma.
--- protractor.conf.js
+++ protractor.conf.js
@@ -1,32 +1,81 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
+// Protractor configuration
+// https://github.com/angular/protractor/blob/master/referenceConf.js
 
-/*global jasmine */
-var SpecReporter = require('jasmine-spec-reporter');
+'use strict';
 
-exports.config = {
-  allScriptsTimeout: 11000,
+var config = {
+  // The timeout for each script run on the browser. This should be longer
+  // than the maximum time your application needs to stabilize between tasks.
+  allScriptsTimeout: 110000,
+
+  // A base URL for your application under test. Calls to protractor.get()
+  // with relative paths will be prepended with this.
+  baseUrl: 'http://localhost:' + (process.env.PORT || '9000'),
+
+  // Credientials for Saucelabs
+  sauceUser: process.env.SAUCE_USERNAME,
+
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+  // list of files / patterns to load in the browser
   specs: [
-    './e2e/**/*.e2e-spec.ts'
+    'e2e/**/*.spec.js'
   ],
+
+  // Patterns to exclude.
+  exclude: [],
+
+  // ----- Capabilities to be passed to the webdriver instance ----
+  //
+  // For a full list of available capabilities, see
+  // https://code.google.com/p/selenium/wiki/DesiredCapabilities
+  // and
+  // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'name': 'Fullstack E2E',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER
   },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
+
+  // ----- The test framework -----
+  //
+  // Jasmine and Cucumber are fully supported as a test and assertion framework.
+  // Mocha has limited beta support. You will need to include your own
+  // assertion framework if working with mocha.
+  framework: 'mocha',
+
+  // ----- Options to be passed to mocha -----
+  mochaOpts: {
+    reporter: 'spec',
+    timeout: 30000,
+    defaultTimeoutInterval: 30000
   },
-  useAllAngular2AppRoots: true,
-  beforeLaunch: function() {
-    require('ts-node').register({
-      project: 'e2e'
-    });
+
+  // Prepare environment for tests
+  params: {
+    serverConfig: require('./server/config/environment')
   },
+
   onPrepare: function() {
-    jasmine.getEnv().addReporter(new SpecReporter());
+    require('babel-register');
+    // Load Mocha and Chai + plugins
+    require('./mocha.conf');
+
+    // Expose should assertions (see https://github.com/angular/protractor/issues/633)
+    Object.defineProperty(
+      protractor.promise.Promise.prototype,
+      'should',
+      Object.getOwnPropertyDescriptor(Object.prototype, 'should')
+    );
+
+    var serverConfig = config.params.serverConfig;
+
+    // Setup mongo for tests
+    var mongoose = require('mongoose');
+    mongoose.connect(serverConfig.mongo.uri, serverConfig.mongo.options); // Connect to database
   }
 };
+
+config.params.baseUrl = config.baseUrl;
+exports.config = config;
