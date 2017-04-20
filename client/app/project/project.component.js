df97dd2ac99f3acdb8dbdc3a67d18ecc37321b0e
Will Hess <willh@aloompa.com>
2017-04-20 09:10:41 -0500

    Proper Chart Type

--- client/app/project/project.component.js
+++ client/app/project/project.component.js
@@ -38,43 +38,82 @@ export class ProjectComponent {
 
   renderBurndown() {
     var ctx = document.getElementById("burndown");
-    var myChart = new Chart(ctx, {
-    type: 'bar',
+    var scatterChart = new Chart(ctx, {
+    type: 'line',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'Scatter Dataset',
+            data: [{
+                x: 10,
+                y: -10
+            }, {
+                x: 8,
+                y: -6
+            }, {
+                x: 5,
+                y: -2
+            }, {
+                x: 4,
+                y: 1
+            }, {
+                x: 3,
+                y: 2
+            }, {
+                x: 2,
+                y: 3
+            }, {
+                x: 0,
+                y: 4
+            }]
         }]
     },
     options: {
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
             }]
         }
     }
 });
 }
+//     var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255,99,132,1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         }
+//     }
+// });
+// }
 
 
 
