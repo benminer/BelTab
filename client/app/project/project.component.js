cd761c1736884c0f102f5afe2f3fb30bc8cd4eaf
Corey Sery <coreysery@gmail.com>
2017-04-24 16:12:10 -0500

    Revert "formatting"
    This reverts commit 4244214058898d3f950bb7cf3a89ad0c2b9d91c5.

--- client/app/project/project.component.js
+++ client/app/project/project.component.js
@@ -53,36 +53,42 @@ export class ProjectComponent {
 		let ctx = document.getElementById('burndownChart');
 
 		this.burndownChart = new Chart(ctx, {
-			type: 'line',
+      type: 'line',
 			data: {
-				labels: Array(11).fill(0).map((_, i) => i + 1),
-				datasets: [{
-					label: 'Actual',
-					data: [100, 95, 80, 69, 53, 42],
-					borderColor: '#26C6DA',
-					backgroundColor: 'rgba(0,188,212,0.2)',
-				}, {
-					label: 'Pace',
-					borderColor: '#BDBDBD',
-					backgroundColor: 'rgba(0,0,0,0)',
-					data: [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0],
-				}, {
-					label: 'Predicted',
-					borderColor: '#80DEEA',
-					backgroundColor: 'rgba(0,0,0,0)',
-					data: [100, 95, 80, 69, 53, 42, 30, 20, 11, 5, 0]
-				}],
-			},
+        labels: Array(11).fill(0).map((_,i) => i+1),
+        // labels: ['asd', 'asd','asdf','asdf'],
+        datasets: [{
+          label: 'Actual',
+          data: [ 100, 95, 80, 69, 53, 42 ],
+          borderColor: '#26C6DA',
+          backgroundColor: 'rgba(0,188,212,0.2)',
+  			}, {
+          label: 'Pace',
+          borderColor: '#BDBDBD',
+          backgroundColor: 'rgba(0,0,0,0)',
+          data: [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0],
+        }, {
+          label: 'Predicted',
+          borderColor: '#80DEEA',
+          backgroundColor: 'rgba(0,0,0,0)',
+          data: [ 100, 95, 80, 69, 53, 42, 30, 20, 11, 5, 0 ]
+        }],
+      },
 			options: {
-				scales: {
-					yAxes: [{
-						ticks: { beginAtZero: true }
-					}],
-					xAxes: [{
-						ticks: { beginAtZero: true }
-					}]
-				},
-			}
+        // legend: {display: false},
+        scales: {
+          yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                  beginAtZero:true
+                }
+            }]
+        },
+      }
 		});
 =======
   /*@ngInject*/
